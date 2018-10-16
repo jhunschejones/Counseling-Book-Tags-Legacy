5cdfdccde19980f4a24e97af3d98390af7c5a59e
Joshua Hunsche Jones <joshua@hunschejones.com>
2018-10-15 22:09:00 -0700

    Add `/details` endpoint, update search endpoints
    Add `/details` endpoint to access ISBN and description, update 3 origional search endpoints to parse raw API data and return smaller objects, include pagination and relevant error handling

--- routes/search.routes.js
+++ routes/search.routes.js
@@ -3,50 +3,117 @@ const request = require('request')
 const xml2js = require('xml2js')
 const router = express.Router()
 
+// ====== search all books by title ======
 router.get('/title/:title', function(req, res) {
   const title = req.params.title
+  // if page is not set, or not a number, set it to 1
+  const page = parseInt(req.query.page) || 1
 
   request.get(  
   {  
-    url: `https://www.goodreads.com/search/index.xml?key=6TVJIoGvRqax9atpBsJFPw&q=${title}&search[field]=title`
+    url: `https://www.goodreads.com/search/index.xml?key=6TVJIoGvRqax9atpBsJFPw&q=${title}&search[field]=title&page=${page}`
   },  
   (getErr, getRes, getBody) => {  
     if (getErr) {  
-      console.error(getErr)
+      // console.error(getErr)
       res.status(500).send({ "error" : getErr })
       return
     } else { 
-      // res.setHeader("Cache-Control", "private, max-age=600")
       xml2js.parseString(getBody, function (err, result) {
-        res.send(result.GoodreadsResponse.search[0].results);
-        return
+        const query = result.GoodreadsResponse.search[0]
+        const works = query.results[0].work
+        let returnArray = [{ 
+          "totalResults" : query["total-results"][0], 
+          "pageStart" : query["results-start"][0], 
+          "pageEnd" : query["results-end"][0], 
+          "currentPage" : page,
+          "totalPages" : Math.floor(query["total-results"][0] / 19)
+        }]
+
+        try {
+          for (let index = 0; index < works.length; index++) {
+            let element = works[index];
+
+            let bookSearchObject = {
+              "bookid" : element.best_book[0].id[0]._,
+              "title" : element.best_book[0].title[0],
+              "author" : element.best_book[0].author[0].name[0],
+              "published" : element.original_publication_year[0]._,
+              "cover" : element.best_book[0].image_url[0].replace(/m\//g, 'l/').replace(/col\//g, 'com/')
+            }
+
+            returnArray.push(bookSearchObject)
+          }
+          res.send(returnArray)
+          return
+
+        } catch (error) {
+          // error means the query is past the last page
+          returnArray.push({ "message" : "no more pages of results, totalPages and totalResults in response[0] may not be correct" })
+          res.status(500).send(returnArray)
+          return
+        }
       })
     }  
   })
 })
 
+// ====== search all books by author ======
 router.get('/author/:author', function(req, res) {
   const author = req.params.author
+  // if page is not set, or not a number, set it to 1
+  const page = parseInt(req.query.page) || 1
 
   request.get(  
   {  
-    url: `https://www.goodreads.com/search/index.xml?key=6TVJIoGvRqax9atpBsJFPw&q=${author}&search[field]=author`
+    url: `https://www.goodreads.com/search/index.xml?key=6TVJIoGvRqax9atpBsJFPw&q=${author}&search[field]=author&page=${page}`
   },  
   (getErr, getRes, getBody) => {  
     if (getErr) {  
-      console.error(getErr)
+      // console.error(getErr)
       res.status(500).send({ "error" : getErr })
       return
     } else { 
-      // res.setHeader("Cache-Control", "private, max-age=600")
       xml2js.parseString(getBody, function (err, result) {
-        res.send(result.GoodreadsResponse.search[0].results);
-        return
+        const query = result.GoodreadsResponse.search[0]
+        const works = query.results[0].work
+        let returnArray = [{ 
+          "totalResults" : query["total-results"][0], 
+          "pageStart" : query["results-start"][0], 
+          "pageEnd" : query["results-end"][0], 
+          "currentPage" : page,
+          "totalPages" : Math.floor(query["total-results"][0] / 19)
+        }]
+
+        try {
+          for (let index = 0; index < works.length; index++) {
+            let element = works[index];
+
+            let bookSearchObject = {
+              "bookid" : element.best_book[0].id[0]._,
+              "title" : element.best_book[0].title[0],
+              "author" : element.best_book[0].author[0].name[0],
+              "published" : element.original_publication_year[0]._,
+              "cover" : element.best_book[0].image_url[0].replace(/m\//g, 'l/').replace(/col\//g, 'com/')
+            }
+
+            returnArray.push(bookSearchObject)
+          }
+          res.send(returnArray)
+          return
+
+        } catch (error) {
+          // error means the query is past the last page
+          returnArray.push({ "message" : "no more pages of results, totalPages and totalResults in response[0] may not be correct" })
+          res.status(500).send(returnArray)
+          return
+        }
       })
     }  
   })
 })
 
+// ====== search all books by isbn or isbn13 ======
 router.get('/isbn/:isbn', function(req, res) {
   const isbn = parseInt(req.params.isbn)
 
@@ -58,13 +125,30 @@ router.get('/isbn/:isbn', function(req, res) {
     },  
     (getErr, getRes, getBody) => {  
       if (getErr) {  
-        console.error(getErr)
+        // console.error(getErr)
         res.status(500).send({ "error" : getErr })
         return
       } else { 
-        // res.setHeader("Cache-Control", "private, max-age=600")
         xml2js.parseString(getBody, function (err, result) {
-          res.send(result.GoodreadsResponse.search[0].results);
+          // returns an array of works
+          const works = result.GoodreadsResponse.search[0].results[0].work
+          let returnArray = []
+
+          for (let index = 0; index < works.length; index++) {
+            let element = works[index];
+
+            let bookSearchObject = {
+              "bookid" : element.best_book[0].id[0]._,
+              "title" : element.best_book[0].title[0],
+              "author" : element.best_book[0].author[0].name[0],
+              "published" : element.original_publication_year[0]._,
+              "cover" : element.best_book[0].image_url[0].replace(/m\//g, 'l/').replace(/col\//g, 'com/')
+            }
+
+              returnArray.push(bookSearchObject)
+          }
+
+          res.send(returnArray)
           return
         })
       }  
@@ -73,5 +157,55 @@ router.get('/isbn/:isbn', function(req, res) {
     res.status(500).send({"error": "isbn must be a number"})
   }
 })
+
+// ====== use goodreads bookid, obtained through other searches, to find book details ======
+router.get('/details/:bookid', function(req, res) {
+  const bookid = parseInt(req.params.bookid)
+
+  // bookid will have a value of null if it is not a number value  
+  if (bookid) {
+    request.get(  
+    {  
+      url: `https://www.goodreads.com/book/show.xml?key=6TVJIoGvRqax9atpBsJFPw&id=${bookid}`
+    },  
+    (getErr, getRes, getBody) => {  
+      if (getErr) {  
+        // console.error(getErr)
+        res.status(500).send({ "error" : getErr })
+        return
+      } else { 
+        xml2js.parseString(getBody, function (err, result) {
+          const book = result.GoodreadsResponse.book[0]
+          let writers = []
+
+          const authors = book.authors[0].author
+          for (let index = 0; index < authors.length; index++) {
+            const author = authors[index]
+
+            if (author.role[0] == "") { writers.push(author.name[0]) }
+          }
+          
+          let returnObject = {
+            "id" : book.id[0],
+            "title" : book.title[0],
+            "author(s)" : writers,
+            "isbn" : book.isbn[0],
+            "isbn13" : book.isbn13[0],
+            "published" : book.publication_year[0],
+            "publisher" : book.publisher[0],
+            "cover" : book.image_url[0].replace(/m\//g, 'l/').replace(/col\//g, 'com/'),
+            "description" : book.description[0]
+          }
+
+          res.send(returnObject)
+          return
+        })
+      }  
+    })
+  } else {
+    res.status(500).send({"error": "bookid value must be a valid Goodreads book_id"})
+  }
+})  
+
 
 module.exports = router
